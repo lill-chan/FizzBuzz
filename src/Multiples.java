3d3dbd1f32e8e8d6b2752d911a70d4dc93ca0d68
lill-chan <lill.chan@mail.utoronto.ca>
2024-09-11 01:40:57 -0400

    new file Multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int multipleOf3or5 = 0;
+        int i = 1;
+        while (i < 1000) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy3 && divisibleBy5) {
+                multipleOf3or5++;
+                i++;
+            } else if (divisibleBy3) {
+                multipleOf3or5++;
+                i++;
+            } else if (divisibleBy5) {
+                multipleOf3or5++;
+                i++;
+            } else {
+                i++;
+            }
+        }
+        System.out.println(multipleOf3or5);
+
+    }
+
 }
